e91ca35333e34b34b791c2188f1fb0a11a581bf9
Vlad Tancev <vlad.tancev@gmail.com>
2022-02-13 20:24:14 +0300

    Updated var names & add information

--- Video_nn.py
+++ Video_nn.py
@@ -6,75 +6,75 @@ import glob
 from datetime import datetime
 from moviepy.editor import AudioFileClip
 
-class Video_nn:
-    def improveVideo(self, pathToVideo, pathToUpdatedVideo='untitled.avi', *argsRealsr):#Function to improve the video
-        filename = pathToVideo.split('/')[-1]#take filename
-        if pathToUpdatedVideo.split('/')[-1].count('.') == 0:#check path it's file or directory
-            if not os.path.exists(pathToUpdatedVideo):
-                os.mkdir(pathToUpdatedVideo)
-            directory = pathToUpdatedVideo
-            pathToUpdatedVideo += '/untitled.avi'#it's path for a future file
+class VideoNN:
+    def improve_video(self, videofile, upd_videofile='untitled.avi', *args_realsr):#Function to improve the video
+        filename = videofile.split('/')[-1]#take filename
+        if upd_videofile.split('/')[-1].count('.') == 0:#check path it's file or directory
+            if not os.path.exists(upd_videofile):
+                os.mkdir(upd_videofile)
+            directory = upd_videofile
+            upd_videofile += '/untitled.avi'#it's path for a future file
         else:
-            directory = pathToVideo.split(filename)[0]
+            directory = videofile.split(filename)[0]
 
-        pathToFragments = filename.replace('.', '-') + '_fragments'
-        pathToUpdatedFragments = filename.replace('.', '-') + '_updated_fragments'
-        pathToUpdatedVideoWithoutAudio = 'UWOA_' + filename
+        fragments_path = filename.replace('.', '-') + '_fragments'
+        upd_fragments_path = filename.replace('.', '-') + '_updated_fragments'
+        upd_videofile_WOA = 'UWOA_' + filename
 
         if directory:
-            pathToFragments = directory + '/' + pathToFragments
-            pathToUpdatedFragments = directory + '/' + pathToUpdatedFragments
-            pathToUpdatedVideoWithoutAudio = directory + '/' + pathToUpdatedVideoWithoutAudio
-        for path in [pathToFragments, pathToUpdatedFragments]:
+            fragments_path = directory + '/' + fragments_path
+            upd_fragments_path = directory + '/' + upd_fragments_path
+            upd_videofile_WOA = directory + '/' + upd_videofile_WOA
+        for path in [fragments_path, upd_fragments_path]:
             if not os.path.exists(path):
                 os.mkdir(path)
-        if self.videoToFragments(pathToVideo, pathToFragments) != 0:
+        if self.video_to_fragments(videofile, fragments_path) != 0:
             print('Error on function video to fragments')
             return -1
 
-        subprocess.run(['cp', pathToFragments + '/info.txt', pathToFragments + '/audio.mp3', pathToUpdatedFragments])
+        subprocess.run(['cp', fragments_path + '/info.txt', fragments_path + '/audio.mp3', upd_fragments_path])
 
-        finish_returnCode = self.useRealsr(pathToFragments, pathToUpdatedFragments, *argsRealsr)
+        finish_returncode = self.use_realsr(fragments_path, upd_fragments_path, *args_realsr)
 
-        if finish_returnCode != 0:
+        if finish_returncode != 0:
             print('Error on upscaling frames')
             return -1
 
-        if self.glueFrames(pathToUpdatedFragments, pathToUpdatedVideoWithoutAudio) != 0:
+        if self.glue_frames(upd_fragments_path, upd_videofile_WOA) != 0:
             print('Error on glue frames')
             return -1
-        if self.addAudio(pathToUpdatedVideoWithoutAudio, pathToFragments + '/audio.mp3', pathToUpdatedVideo) != 0:
+        if self.add_audio(upd_videofile_WOA, fragments_path + '/audio.mp3', upd_videofile) != 0:
             print('Error on adding audio')
             return -1
 
         return 0
 
-    def useRealsr(self, pathToInput, pathToOutput, *argsRealsr, pathToRealsr='./realsr-ncnn-vulkan/realsr-ncnn-vulkan'):
+    def use_realsr(self, input_path, output_path, *args_realsr, realsr_path='./realsr-ncnn-vulkan/realsr-ncnn-vulkan'):
         t1 = datetime.now()
-        finish = subprocess.run([pathToRealsr, '-i', pathToInput, '-o', pathToOutput, *argsRealsr])
+        finish = subprocess.run([realsr_path, '-i', input_path, '-o', output_path, *args_realsr])
         t2 = datetime.now()
         print('time cost realsr =', t2 - t1)
         return finish.returncode
 
-    def videoToFragments(self, path, toPath=None):
+    def video_to_fragments(self, path, output_path=None):
         # check paths
         if not os.path.exists(path):
             print(path + " not exists")
             return
         elif os.path.isdir(path):
-            print(toPath + " is not file")
+            print(path + " is not file")
             return
-        elif toPath and toPath.split('/')[-1].count('.') > 0:
-            print(toPath + " is not directory")
+        elif output_path and output_path.split('/')[-1].count('.') > 0:
+            print(output_path + " is not directory")
             return
 
         filename = path.split('/')[-1]
 
-        if not toPath:
-            toPath = filename.replace('.', '-') + "_fragments"
-        if not os.path.exists(toPath):
-            os.mkdir(toPath)
-        if os.listdir(toPath):
+        if not output_path:
+            output_path = filename.replace('.', '-') + "_fragments"
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        if os.listdir(output_path):
             print('WARNING!!! Path for fragments is not empty. Files with the same name will be overwritten')
 
         t1 = datetime.now()
@@ -89,74 +89,73 @@ class Video_nn:
             ret, frame = videoCapture.read()
             if ret:
                 count_frames += 1
-                cv2.imwrite("%s/%d.png" % (toPath, i), frame)
+                cv2.imwrite("%s/%d.png" % (output_path, i), frame)
         t2 = datetime.now()
         if count_frames != frames:
             frames = count_frames
         print('time cost  = ', t2 - t1)
 
         # create a txt file with additional info for processing by other programs
-        with open(toPath + '/info.txt', 'w') as infoFile:
+        with open(output_path + '/info.txt', 'w') as infoFile:
             infoFile.write(str(int(fps)) + '\n')  # fps
             infoFile.write(filename + '\n')  # filename
             infoFile.write(str(int(frames)))  # frames
         # catching audio
         try:
             audioclip = AudioFileClip(path)
-            audioclip.write_audiofile(toPath + '/audio.mp3')
+            audioclip.write_audiofile(output_path + '/audio.mp3')
             audioclip.close()
         except:
             print('video without audio')
 
         return 0
 
-    def glueFrames(self, pathToSrc, pathToVideo='untitled.avi', codec='h264', fps=30, *argsFffmpeg, photoExtension='png'):
-        frames = glob.glob(pathToSrc + '/*.' + photoExtension)
+    def glue_frames(self, src_path, videofile='untitled.avi', codec='h264', fps=30, *args_ffmpeg, photo_extenstion='png'):
+        frames = glob.glob(src_path + '/*.' + photo_extenstion)
         if len(frames) == 0:
-            print(f'Frames with extension {photoExtension} not found')
+            print(f'Frames with extension {photo_extenstion} not found')
             return
-        frameSize = cv2.imread(frames[0]).shape[1::-1]
+        # frameSize = cv2.imread(frames[0]).shape[1::-1]
         filename = 'untitled.avi'
 
-        if os.path.exists(pathToSrc + '/info.txt'):
-            with open(pathToSrc + '/info.txt', 'r') as infoFile:
+        if os.path.exists(src_path + '/info.txt'):
+            with open(src_path + '/info.txt', 'r') as infoFile:
                 try:
                     fps = int(infoFile.readline())
                     filename = 'UpdWOA_' + infoFile.readline().strip()#Updated without audio
-                    countFrames = int(infoFile.readline())
-                    if countFrames != len(frames):
+                    count_frames = int(infoFile.readline())
+                    if count_frames != len(frames):
                         print('The number of files in info.txt does not match the actual')
                 except:
                     print("Bad info.txt")
         else:
             print("WARNING!!! info.txt not exists. It's true path?")
 
-        if pathToVideo.split('/')[-1].count('.') == 0:
-            pathToVideo += '/' + filename
+        if videofile.split('/')[-1].count('.') == 0:
+            videofile += '/' + filename
 
         t1 = datetime.now()
 
-        finish = subprocess.run(['ffmpeg', '-start_number', '1', '-r', str(fps), '-i', pathToSrc + '/%d.png', '-vcodec', codec, '-y', *argsFffmpeg, pathToVideo])
+        finish = subprocess.run(['ffmpeg', '-start_number', '1', '-r', str(fps), '-i', src_path + '/%d.png', '-vcodec', codec, '-y', *args_ffmpeg, videofile])
 
         t2 = datetime.now()
         print('time cost qlue =', t2 - t1)
 
-        # addAudio(pathToVideo, pathToSrc + '/audio.mp3', 'videos/finale_barabans.avi')#потом надо будет
         return finish.returncode
 
-    def addAudio(self, pathToVideo, pathToAudio, newName=None):
-        if not newName:
-            newName = 'UPDATED_' + pathToVideo.split('/')[-1]
-        if pathToVideo in (newName, pathToAudio):
+    def add_audio(self, videofile, audio_path, new_name=None):
+        if not new_name:
+            new_name = 'UPDATED_' + videofile.split('/')[-1]
+        if videofile in (new_name, audio_path):
             print('Files with same name')
             return
         t1 = datetime.now()
-        finish = subprocess.run(['ffmpeg', '-i', pathToAudio, '-i', pathToVideo, '-codec', 'copy', '-y', newName])
+        finish = subprocess.run(['ffmpeg', '-i', audio_path, '-i', videofile, '-codec', 'copy', '-y', new_name])
         t2 = datetime.now()
         print('time cost add audio = ', t2 -t1)
         return finish.returncode
 
 if __name__ == '__main__':
-    video_nn = Video_nn()
+    video_nn = VideoNN()
     # video_nn.improveVideo('videos/crop_barabans.mp4', 'videos/cropped_fragments')
-    video_nn.improveVideo(*sys.argv[1:])
+    video_nn.improve_video(*sys.argv[1:])
--- server_nn.py
+++ server_nn.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import werkzeug
-from Video_nn import Video_nn
+from Video_nn import VideoNN
 import os
 import time
 import threading
@@ -14,81 +14,98 @@ parser = reqparse.RequestParser()
 parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
 
 
-def checkInfo(pathToInfo):
-    if not os.path.isdir(pathToInfo):
-        os.mkdir(pathToInfo)
-    infoFiles = ['info.txt']
-    for file in infoFiles:
-        if not os.path.exists(pathToInfo + file):
+def check_info(info_path):
+    if not os.path.isdir(info_path):
+        os.mkdir(info_path)
+    info_files = ['all_updated.txt', 'new_updated.txt', 'order.txt']
+    for file in info_files:
+        if not os.path.exists(info_path + file):
             open(file, 'w')
 
 class HellowWOrld(Resource):
     def __init__(self):
-        self.isBusyProceccing = False
-        self.informationPath = 'information/'
-        checkInfo(self.informationPath)
+        self.is_proceccing = False
+        self.information_path = 'information/'
+        self.content_path = 'content/'
+        check_info(self.information_path)
 
-    def post(self, picturesFolderName):
-        contentPath = 'content/'
-        pathToPictures = contentPath + picturesFolderName
-        pathToUpdatedPictures = contentPath + 'Updated_' + picturesFolderName
-        if not os.path.exists(pathToPictures):
-            os.mkdir(pathToPictures)
-        if not os.path.exists(pathToUpdatedPictures):
-            os.mkdir(pathToUpdatedPictures)
+    def post(self, pictures_folder):
+        pictures_path = self.content_path + pictures_folder
+        upd_pictures_path = self.content_path + 'Updated_' + pictures_folder
+        if not os.path.exists(pictures_path):
+            os.mkdir(pictures_path)
+        if not os.path.exists(upd_pictures_path):
+            os.mkdir(upd_pictures_path)
+
         args = parser.parse_args()
         picture = args['picture']
-        argsRealsr = []
+        args_realsr = ''.split()
 
         if picture:
-            if not self.isBusyProceccing:
-                self.process_picture(picture, pathToPictures, pathToUpdatedPictures, *argsRealsr)
+            if not self.is_proceccing:
+                future_filename = 'Updated_' + pictures_folder + '/' \
+                                  + self.process_picture(picture, pictures_path, upd_pictures_path, *args_realsr)
+
+                self.after_processing_thread = threading.Thread(target=self.after_processing,
+                                                                args=(self, future_filename,),
+                                                                name='after_proccessing')
+                self.after_processing_thread.start()
             else:
                 return {'status': 'Wait some times. Computer is busy now by other picture'}
-            return {"status": "Picture was uploaded"}
+
+            return {"status": "Picture was uploaded", 'futureName': future_filename}
         else:
             return {"status": "No picture in request"}
 
-    def process_picture(self, picture, pathToPictures, pathToUpdatedPictures, *argsRealsr):
-        self.addToOrder(picture)
-        filenameWOExtension = picture.filename.split('.')[0]
-        picture.save('{}/{}'.format(pathToPictures, picture.filename))
+    def process_picture(self, picture, pictures_path, upd_pictures_path, *args_realsr):
+        self.add_information(pictures_path + picture.filename)
+        filenameWOE = picture.filename.split('.')[0]
+        picture.save('{}/{}'.format(pictures_path, picture.filename))
 
-        thread = threading.Thread(target=Video_nn.useRealsr, args=(Video_nn, pathToPictures + '/' + picture.filename,
-                                                                   pathToUpdatedPictures + '/' + filenameWOExtension + '.png',
-                                                                   *argsRealsr,),
-                                  kwargs={
-                                      'pathToRealsr': '/home/vladt/PycharmProjects/video_nn/realsr-ncnn-vulkan/realsr-ncnn-vulkan'})
-        thread.start()
+        self.processing_thread = threading.Thread(target=VideoNN.use_realsr,
+                                                  args=(VideoNN, pictures_path + '/' + picture.filename,
+                                                           upd_pictures_path + '/' + filenameWOE + '.png',
+                                                           *args_realsr,),
+                                  kwargs= {
+                                      'pathToRealsr': '/home/vladt/PycharmProjects/VideoNN/realsr-ncnn-vulkan/realsr-ncnn-vulkan'},
+                                             name='processing')
 
+        self.processing_thread.start()
+        return filenameWOE + '.png'
 
-    def addToOrder(self, pictureName, first=True):
-        checkInfo(self.informationPath)
-        infoFilePath = self.informationPath + 'info.txt'
-        with open(infoFilePath, 'r+') as file:
+    def after_processing(self, picture_filename):
+        while not self.processing_thread.is_alive():
+            self.add_information(picture_filename, 'all_updated.txt')
+            self.add_information(picture_filename, 'new_updated.txt')
+            self.delete_information('order.txt')
+
+    def add_information(self, picture_filename, infofile, first=True):
+        check_info(self.information_path)
+        file_path = self.information_path + infofile
+        with open(file_path, 'r+') as file:
             text = file.readlines()
             if first:
-                text = [pictureName + '\n'] + text
+                text = [picture_filename + '\n'] + text
             else:
                 if (text[-1] == '\n') or (not text):
                     del text[-1]
-                text += [pictureName]
+                text += [picture_filename]
             file.seek(0)
             for row in text:
                 file.write(row)
 
-    def deleteFromOrder(self):
-        checkInfo(self.informationPath)
-        infoFilePath = self.informationPath + 'info.txt'
-        with open(infoFilePath, 'r+') as file:
+    def delete_information(self, infofile):
+        check_info(self.information_path)
+        file_path = self.information_path + infofile
+        with open(file_path, 'r+') as file:
             text = file.readlines()
             text = text[1:]
             file.seek(0)
             for row in text:
                 file.write(row)
-    # def delete(self, picturesFolderName):
+    # def delete(self, pictures_folder):
 
-api.add_resource(HellowWOrld, '/<string:picturesFolderName>')
+api.add_resource(HellowWOrld, '/content/<string:pictures_folder>')
 
 if __name__ == "__main__":
     app.run(debug=True)
