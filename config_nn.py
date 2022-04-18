5208b0bbc2cd31ed2c35bd189c08db25edb41cc5
ElectroForez <vlad.tancev@gmail.com>
2022-04-18 19:07:25 +0300

    change names

--- config_nn.py
+++ config_nn.py
@@ -1,5 +1,5 @@
 from os import environ
-PASSWORD = environ['PASS_API_NN_SERV']
+PASSWORD = environ['PASS_NN']
 REALSR_PATH = environ['REALSR_PATH']
 INFORMATION_PATH = 'information/'
 CONTENT_PATH = 'content/'
